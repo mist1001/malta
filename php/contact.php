3df8e7fe447d270ce9bf14bcb195aa64c52c37b2
mist1001 <57133727+mist1001@users.noreply.github.com>
2019-10-29 15:50:21 +0300

    Update contact.php

--- php/contact.php
+++ php/contact.php
@@ -57,7 +57,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@example.net";
-$address = "example@example.net";
+$address = "amigos2725@gmail.com";
 
 
 // Configuration option.
